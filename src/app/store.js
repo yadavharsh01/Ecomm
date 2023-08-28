5669d506550b768ad82e224f932bb59d68d0eac5
harsh <harshyadav.hy20@gmail.com>
2023-08-28 12:10:04 +0530

    login signup cart added

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, createReducer } from '@reduxjs/toolkit';
+import productReducer from '../features/product/productSlice';
+import authReducer from '../features/auth/authSlice';
+import cartReducer from '../features/cart/cartSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    product: productReducer,
+    auth: authReducer,
+    cart: cartReducer
   },
 });
